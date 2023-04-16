40a8847b47925124dd554b9d53f2d93b098fbb0c
Juha Kervinen <juha@kokopino.com>
2023-04-16 07:07:12 +0300

    Installer first attempt

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 set -e 
 echo "Installing to /opt/niberator.."
+mkdir -p /opt/niberator && touch /opt/niberator/write-test.txt
+if [ $? -ne 0 ]; then
+    echo "Cannot write to /opt/niberator. Please run as root or sudo."
+    exit 1
+fi
+rm /opt/niberator/write-test.txt
+systemd --version
+if [ $? -ne 0 ]; then
+    echo "Systemd not found. Please install systemd."
+    exit 1
+fi
+cp switch-speed-by-hat-input.py /opt/niberator
+cp switch-speed-by-hat-input.service /etc/systemd/system
+systemctl daemon reload
+echo "Installed."
